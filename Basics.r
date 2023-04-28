b9e483d644815d56ea1197672a533eb867de72da
Ashutosh Dubey <93329307+ashu10dubey@users.noreply.github.com>
2023-04-28 23:41:25 +0530

    Add files via upload

---
+++ Basics.r
@@ -0,0 +1,27 @@
+# This code prints the message "Hello, world!" to the console
+print("Hello, world!")
+
+# This code creates a variable called "x" and assigns it the value 10
+x <- 10
+
+# This code performs basic arithmetic operations on two variables
+x <- 10
+y <- 5
+sum <- x + y
+diff <- x - y
+prod <- x * y
+quot <- x / y
+
+# This code creates a sequence of numbers from 1 to 10
+seq(1, 10)
+
+# This code creates a vector of values
+vec <- c(1, 2, 3, 4, 5)
+
+# This code subsets a vector to get the first two values
+vec <- c(1, 2, 3, 4, 5)
+vec[1:2]
+
+# This code creates a 3x3 matrix
+mat <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, ncol = 3)
+
